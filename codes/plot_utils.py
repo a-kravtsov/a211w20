05533c27af91bc70d0fe63f232c4d4e8e0c7e5b2
Andrey Kravtsov <andrey@DESKTOP-C84UTDS.localdomain>
2020-02-10 01:12:35 -0600

    a small fix in 04_histograms, added hw03_background_sdss with SDSS survey info.

--- codes/plot_utils.py
+++ codes/plot_utils.py
@@ -100,3 +100,64 @@ def plot_histogram(data, bins=None, xlabel=' ', ylabel=' ', tickmarks = False,
         plt.title(plot_title, fontsize=3.*figsize)
 
     plt.show()
+
+import scipy.optimize as opt
+from matplotlib.colors import LogNorm
+
+def conf_interval(x, pdf, conf_level):
+    return np.sum(pdf[pdf > x])-conf_level
+
+def plot_2d_dist(x,y, xlim, ylim, nxbins, nybins, figsize=(5,5), 
+                cmin=1.e-4, cmax=1.0, 
+                log=False, weights=None, xlabel='x',ylabel='y', 
+                clevs=None, fig_setup=None, savefig=None):
+    """
+    construct and plot a binned, 2d distribution in the x-y plane 
+    using nxbins and nybins in x- and y- direction, respectively
+    
+    log = specifies whether logged quantities are passed to be plotted on log-scale outside this routine
+    """
+    if fig_setup is None:
+        fig, ax = plt.subplots(figsize=figsize)
+        plt.ylabel(ylabel)
+        plt.xlabel(xlabel)
+        plt.xlim(xlim[0], xlim[1])
+        plt.ylim(ylim[0], ylim[1])
+    else:
+        ax = fig_setup
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_xlim(xlim); ax.set_ylim(ylim)
+
+    if xlim[1] < 0.: ax.invert_xaxis()
+
+    if weights is None: weights = np.ones_like(x)
+    H, xbins, ybins = np.histogram2d(x, y, weights=weights, bins=(np.linspace(xlim[0], xlim[1], nxbins),np.linspace(ylim[0], ylim[1], nybins)))
+    
+    H = np.rot90(H); H = np.flipud(H); 
+             
+    X,Y = np.meshgrid(xbins[:-1],ybins[:-1]) 
+
+    H = H/np.sum(H)        
+    Hmask = np.ma.masked_where(H==0,H)
+    
+    if log:
+        X = np.power(10.,X); Y = np.power(10.,Y)
+
+    pcol = ax.pcolormesh(X, Y,(Hmask), vmin=cmin*np.max(Hmask), vmax=cmax*np.max(Hmask), cmap=plt.cm.BuPu, norm = LogNorm(), linewidth=0., rasterized=True)
+    pcol.set_edgecolor('face')
+    
+    # plot contours if contour levels are specified in clevs 
+    if clevs is not None:
+        lvls = []
+        for cld in clevs:  
+            sig = opt.brentq( conf_interval, 0., 1., args=(H,cld) )   
+            lvls.append(sig)
+        
+        ax.contour(X, Y, H, linewidths=(1.0,0.75, 0.5, 0.25), colors='black', levels = sorted(lvls), 
+                norm = LogNorm(), extent = [xbins[0], xbins[-1], ybins[0], ybins[-1]])
+    if savefig:
+        plt.savefig(savefig,bbox_inches='tight')
+    if fig_setup is None:
+        plt.show()
+    return
