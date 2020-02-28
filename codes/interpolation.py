5c0b32f2e7d38357ba5c15d6541f447282272b83
Andrey Kravtsov <andrey@DESKTOP-C84UTDS.localdomain>
2020-02-28 15:37:19 -0600

    added 06d_multid notebook

--- codes/interpolation.py
+++ codes/interpolation.py
@@ -11,3 +11,56 @@ def chebyshev_nodes2(a, b, N):
     assert(b>a)
     return a + 0.5*(b-a)*(1. + np.cos(np.arange(N+1)*np.pi/N))
     
+def polyfit2d(xtr, ytr, ftr, kx=3, ky=3, order=None):
+    '''
+    Two dimensional polynomial fitting by least squares.
+    Fits the functional form f(x,y) = z.
+
+    Notes
+    -----
+    Resulting fit can be plotted with:
+    np.polynomial.polynomial.polygrid2d(x, y, soln.reshape((kx+1, ky+1)))
+
+    Parameters:
+    ----------
+    xtr, ytr: array-like, 1d
+        xtr and ytr coordinates.
+    ftr: 2d numpy array
+        f(xgtr, ygtr) values evaluated on meshgrid of xtr and ytr vectors to fit by polynomial
+    kx, ky: int, default is 3
+        Polynomial order in x and y, respectively.
+    order: int or None, default is None
+        If None, all coefficients up to maxiumum kx, ky, ie. up to and including x^kx*y^ky, are considered.
+        If int, coefficients up to a maximum of kx+ky <= order are considered.
+
+    Returns:
+    -------
+    Return paramters from np.linalg.lstsq.
+
+    soln: np.ndarray
+        Array of polynomial coefficients.
+    residuals: np.ndarray
+    rank: int
+    s: np.ndarray
+
+    '''
+
+    # grid coords
+    x, y = np.meshgrid(xtr, ytr)
+    # coefficient array, up to x^kx, y^ky
+    coeffs = np.ones((kx+1, ky+1))
+
+    # solve array
+    V = np.zeros((coeffs.size, x.size))
+
+    # construct Vandermonde matrix: for each coefficient produce array x^i, y^j
+    for index, (j, i) in enumerate(np.ndindex(coeffs.shape)):
+        # do not include powers greater than order
+        if order is not None and i + j > order:
+            arr = np.zeros_like(x)
+        else:
+            arr = coeffs[i, j] * x**i * y**j
+        V[index] = arr.flatten()
+        
+    # do leastsq fitting and return leastsq result
+    return np.linalg.lstsq(V.T, np.ravel(ftr), rcond=None)[0]
